266e4d1d88e8e92da220d913fe719df9c7678dae
trevid <mingya_dai@outlook.com>
2019-11-23 21:15:40 +0800

    代码格式化

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import shutil
 
 def isDir(dirPath):
     return os.path.isdir(dirPath)
+
+
 def isFile(filePath):
     return os.path.isfile(filePath)
+
 
 # 自动整理文件
 def autoFile(path):
@@ -24,10 +27,10 @@ def autoFile(path):
             #     shutil.move(absPath, oldDir)    # 超过指定时间 移动到old文件夹内
             #     continue
 
-            type = util.get_FileType(absPath)      # 判断类型
-            if (type == None):                      # 类型为None 移动到Other文件夹内
+            type = util.get_FileType(absPath)  # 判断类型
+            if (type == None):  # 类型为None 移动到Other文件夹内
                 type = "Other"
-            else:                                   # 类型不为None
+            else:  # 类型不为None
                 print(absPath + " " + type)
 
             # 判断是否存在对应类型的文件夹 若不存在则新建
@@ -42,12 +45,12 @@ def autoFloder(path):
     floderPath = os.path.join(path, "floder")
     if (not os.path.exists(oldDir)):
         os.mkdir(oldDir)
-    if(not os.path.exists(floderPath)):
+    if (not os.path.exists(floderPath)):
         os.mkdir(floderPath)
 
     for floderName in os.listdir(path):
         absPath = os.path.join(path, floderName)
-        if(floderName in data["type"]["types"] or floderName in data["exclude"]):
+        if (floderName in data["type"]["types"] or floderName in data["exclude"]):
             continue
 
         # print(time.time() - float(util.get_FileModifyTime(absPath) > delay))
@@ -60,13 +63,11 @@ def autoFloder(path):
                 shutil.move(absPath, floderPath)
 
 
-
-
 fileConfig = open("./config.json", 'r', encoding='utf-8')
 data = json.loads(fileConfig.read())
 
-if(data["delay"] != None ):
-    delay = data["delay"]*24*60*60
+if (data["delay"] != None):
+    delay = data["delay"] * 24 * 60 * 60
 else:
     delay = 999999999999999
 
@@ -74,6 +75,3 @@ for path in data["path"]:
     print(os.path.isdir(path))
     autoFile(path)
     autoFloder(path)
-
-
-
